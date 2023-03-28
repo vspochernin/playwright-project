e32f088dadc2929a54fb1a9bb19226c3acbac954
Vladislav Pochernin <vspochernin@gmail.com>
2023-03-28 15:38:42 +0300

    initial commit for maven project

---
+++ src/main/java/ru/vk/company/Main.java
@@ -0,0 +1,10 @@
+package ru.vk.company;
+
+/**
+ * @author pochernin-vla
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/test/java/ru/vk/company/MainTest.java
@@ -0,0 +1,17 @@
+package ru.vk.company;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void positiveTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void negativeTest() {
+        assertTrue(false);
+    }
+}
